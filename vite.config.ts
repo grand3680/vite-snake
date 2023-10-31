b0d314f978da7dd60ba5035a0cd1a35a1ee2de36
grand3680 <kirill.burtsev07@gmail.com>
2023-10-31 17:38:36 +0300

    ts and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  root: './src',
+  build: {
+    outDir: '../dist',
+    target: 'esnext',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
   plugins: [react(), viteSingleFile()],
 })
